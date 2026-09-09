bf7d11d2106d76c8003cdea956cbd055745069d5
czarina28 <maria@mariaworld.com>
2026-09-09 16:28:58 +0200

    Add puzzles 036 through 040

---
+++ puzzles-36-40.js
@@ -0,0 +1,57 @@
+PUZZLES.push(
+    {
+        id: 36,
+        title: "Puzzle 036",
+        difficulty: "easy",
+        groups: [
+            { connection: "THE ANIMALS SONGS", cards: ["House of the Rising Sun", "We Gotta Get Out of This Place", "Don't Let Me Be Misunderstood", "It's My Life"] },
+            { connection: "CAROLE KING SONGS", cards: ["It's Too Late", "I Feel the Earth Move", "So Far Away", "You've Got a Friend"] },
+            { connection: "FAMOUS ROCK PIANISTS", cards: ["Elton John", "Billy Joel", "Jerry Lee Lewis", "Leon Russell"] },
+            { connection: "BANDS WITH 'RED' IN THEIR NAMES", cards: ["Red Hot Chili Peppers", "Simply Red", "King Crimson", "Redbone"] }
+        ]
+    },
+    {
+        id: 37,
+        title: "Puzzle 037",
+        difficulty: "easy",
+        groups: [
+            { connection: "THE SUPREMES SONGS", cards: ["Baby Love", "Stop! In the Name of Love", "You Can't Hurry Love", "Where Did Our Love Go"] },
+            { connection: "THE SEX PISTOLS SONGS", cards: ["Anarchy in the U.K.", "God Save the Queen", "Pretty Vacant", "Holidays in the Sun"] },
+            { connection: "FAMOUS MUSICIANS NAMED STEVIE", cards: ["Stevie Wonder", "Stevie Nicks", "Stevie Ray Vaughan", "Stevie Winwood"] },
+            { connection: "BANDS WITH 'SOUND' IN THEIR NAMES", cards: ["Soundgarden", "Sounds Incorporated", "The Sound", "LCD Soundsystem"] }
+        ]
+    },
+    {
+        id: 38,
+        title: "Puzzle 038",
+        difficulty: "medium",
+        groups: [
+            { connection: "THE YARDBIRDS SONGS", cards: ["For Your Love", "Heart Full of Soul", "Shapes of Things", "Over Under Sideways Down"] },
+            { connection: "KATE BUSH SONGS", cards: ["Wuthering Heights", "Running Up That Hill", "Babooshka", "Cloudbusting"] },
+            { connection: "GUITARISTS WHO PLAYED IN THE YARDBIRDS", cards: ["Eric Clapton", "Jeff Beck", "Jimmy Page", "Chris Dreja"] },
+            { connection: "BANDS WITH 'HEART' IN THEIR NAMES", cards: ["Heart", "Broken Social Scene", "The Heartbreakers", "Bleeding Heart Band"] }
+        ]
+    },
+    {
+        id: 39,
+        title: "Puzzle 039",
+        difficulty: "easy",
+        groups: [
+            { connection: "THE TEMPTATIONS SONGS", cards: ["My Girl", "Papa Was a Rollin' Stone", "Ain't Too Proud to Beg", "Just My Imagination"] },
+            { connection: "NEIL YOUNG SONGS", cards: ["Heart of Gold", "Old Man", "Cinnamon Girl", "Rockin' in the Free World"] },
+            { connection: "FAMOUS CANADIAN ARTISTS", cards: ["Joni Mitchell", "Leonard Cohen", "Neil Young", "Gordon Lightfoot"] },
+            { connection: "BANDS WITH 'GIRL' OR 'GIRLS' IN THEIR NAMES", cards: ["Spice Girls", "Indigo Girls", "Girlschool", "Girl in Red"] }
+        ]
+    },
+    {
+        id: 40,
+        title: "Puzzle 040",
+        difficulty: "medium",
+        groups: [
+            { connection: "JEFFERSON AIRPLANE SONGS", cards: ["Somebody to Love", "White Rabbit", "Volunteers", "Crown of Creation"] },
+            { connection: "BOB MARLEY SONGS", cards: ["No Woman, No Cry", "Three Little Birds", "Redemption Song", "Jamming"] },
+            { connection: "ARTISTS WHO PLAYED MONTEREY POP", cards: ["Jimi Hendrix", "Janis Joplin", "Otis Redding", "Ravi Shankar"] },
+            { connection: "BANDS WITH 'STAR' IN THEIR NAMES", cards: ["Big Star", "Starship", "Shooting Star", "Starsailor"] }
+        ]
+    }
+);
