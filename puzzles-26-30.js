2b5a521e7cecc643fda1767d4fd8049c6e121e9d
czarina28 <maria@mariaworld.com>
2026-09-09 16:27:08 +0200

    Add puzzles 026 through 030

---
+++ puzzles-26-30.js
@@ -0,0 +1,57 @@
+PUZZLES.push(
+    {
+        id: 26,
+        title: "Puzzle 026",
+        difficulty: "easy",
+        groups: [
+            { connection: "THE BEACH BOYS SONGS", cards: ["Good Vibrations", "California Girls", "God Only Knows", "Wouldn't It Be Nice"] },
+            { connection: "JANIS JOPLIN SONGS", cards: ["Piece of My Heart", "Cry Baby", "Mercedes Benz", "Me and Bobby McGee"] },
+            { connection: "FAMOUS MUSIC CITIES", cards: ["Memphis", "Nashville", "Detroit", "Liverpool"] },
+            { connection: "BANDS WITH DAYS OR TIMES IN THEIR NAMES", cards: ["Green Day", "Happy Mondays", "The Sundays", "Three Days Grace"] }
+        ]
+    },
+    {
+        id: 27,
+        title: "Puzzle 027",
+        difficulty: "easy",
+        groups: [
+            { connection: "THE KINKS SONGS", cards: ["You Really Got Me", "Waterloo Sunset", "Lola", "Sunny Afternoon"] },
+            { connection: "JOHNNY CASH SONGS", cards: ["Ring of Fire", "Folsom Prison Blues", "I Walk the Line", "A Boy Named Sue"] },
+            { connection: "FAMOUS WOMEN OF PUNK AND NEW WAVE", cards: ["Debbie Harry", "Patti Smith", "Chrissie Hynde", "Poly Styrene"] },
+            { connection: "BANDS WITH 'BOYS' IN THEIR NAMES", cards: ["The Beach Boys", "Beastie Boys", "Pet Shop Boys", "Backstreet Boys"] }
+        ]
+    },
+    {
+        id: 28,
+        title: "Puzzle 028",
+        difficulty: "easy",
+        groups: [
+            { connection: "BEE GEES SONGS", cards: ["Stayin' Alive", "Night Fever", "How Deep Is Your Love", "More Than a Woman"] },
+            { connection: "AC/DC SONGS", cards: ["Back in Black", "Highway to Hell", "Thunderstruck", "You Shook Me All Night Long"] },
+            { connection: "FAMOUS MUSIC BROTHERS", cards: ["Malcolm Young", "Angus Young", "Noel Gallagher", "Liam Gallagher"] },
+            { connection: "BANDS WITH 'BLACK' IN THEIR NAMES", cards: ["Black Sabbath", "Black Flag", "Black Crowes", "Black Keys"] }
+        ]
+    },
+    {
+        id: 29,
+        title: "Puzzle 029",
+        difficulty: "medium",
+        groups: [
+            { connection: "VELVET UNDERGROUND SONGS", cards: ["Sweet Jane", "Heroin", "Pale Blue Eyes", "I'm Waiting for the Man"] },
+            { connection: "THE STONE ROSES SONGS", cards: ["I Wanna Be Adored", "She Bangs the Drums", "Waterfall", "I Am the Resurrection"] },
+            { connection: "ARTISTS ASSOCIATED WITH CBGB", cards: ["Ramones", "Blondie", "Patti Smith", "Television"] },
+            { connection: "BANDS WITH 'STONE' OR 'STONES' IN THEIR NAMES", cards: ["The Rolling Stones", "Stone Temple Pilots", "Queens of the Stone Age", "Sly and the Family Stone"] }
+        ]
+    },
+    {
+        id: 30,
+        title: "Puzzle 030",
+        difficulty: "easy",
+        groups: [
+            { connection: "SIMON & GARFUNKEL SONGS", cards: ["The Sound of Silence", "Mrs. Robinson", "The Boxer", "Bridge Over Troubled Water"] },
+            { connection: "EURYTHMICS SONGS", cards: ["Sweet Dreams", "Here Comes the Rain Again", "Would I Lie to You?", "There Must Be an Angel"] },
+            { connection: "FAMOUS MUSICAL PARTNERSHIPS", cards: ["Lennon & McCartney", "Jagger & Richards", "Bacharach & David", "Goffin & King"] },
+            { connection: "BANDS WITH 'BLUE' IN THEIR NAMES", cards: ["Blue Öyster Cult", "The Moody Blues", "Blues Traveler", "Deep Blue Something"] }
+        ]
+    }
+);
