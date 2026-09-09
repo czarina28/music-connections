a7860f5725218cd6ca015cf128da3ee2e26c7cef
czarina28 <maria@mariaworld.com>
2026-09-09 22:24:14 +0200

    Add 24 eighties music connection rounds

---
+++ puzzles-101-124.js
@@ -0,0 +1,146 @@
+PUZZLES.push(
+  { id:101, title:"Puzzle 101", difficulty:"easy", groups:[
+    { connection:"THE CURE SONGS", cards:["A Forest","Just Like Heaven","Lovesong","Pictures of You"] },
+    { connection:"DURAN DURAN SONGS", cards:["Rio","Hungry Like the Wolf","The Reflex","Ordinary World"] },
+    { connection:"1980s SYNTH-POP ACTS", cards:["Erasure","Pet Shop Boys","Yazoo","Orchestral Manoeuvres in the Dark"] },
+    { connection:"1980s ALBUMS WITH ONE-WORD TITLES", cards:["Thriller","Purple Rain","Graceland","Hounds of Love"] }
+  ]},
+  { id:102, title:"Puzzle 102", difficulty:"easy", groups:[
+    { connection:"DEPECHE MODE SONGS", cards:["People Are People","Master and Servant","Strangelove","Personal Jesus"] },
+    { connection:"THE SMITHS SONGS", cards:["This Charming Man","Panic","Girlfriend in a Coma","How Soon Is Now?"] },
+    { connection:"1980s FEMALE POP STARS", cards:["Cyndi Lauper","Whitney Houston","Belinda Carlisle","Kim Wilde"] },
+    { connection:"BANDS FORMED IN MANCHESTER", cards:["New Order","The Smiths","The Stone Roses","Happy Mondays"] }
+  ]},
+  { id:103, title:"Puzzle 103", difficulty:"medium", groups:[
+    { connection:"R.E.M. SONGS", cards:["Radio Free Europe","So. Central Rain","The One I Love","Stand"] },
+    { connection:"TALKING HEADS SONGS", cards:["Once in a Lifetime","Burning Down the House","Road to Nowhere","And She Was"] },
+    { connection:"1980s COLLEGE ROCK ACTS", cards:["The Replacements","Hüsker Dü","Violent Femmes","Camper Van Beethoven"] },
+    { connection:"ATHENS, GEORGIA BANDS", cards:["R.E.M.","The B-52's","Pylon","Love Tractor"] }
+  ]},
+  { id:104, title:"Puzzle 104", difficulty:"easy", groups:[
+    { connection:"PRINCE SONGS", cards:["1999","Little Red Corvette","Kiss","Raspberry Beret"] },
+    { connection:"MADONNA SONGS", cards:["Holiday","Like a Virgin","Material Girl","Like a Prayer"] },
+    { connection:"1980s SUPERSTAR ALBUMS", cards:["Thriller","Purple Rain","Like a Virgin","Born in the U.S.A."] },
+    { connection:"1980s SONGS WITH WOMEN'S NAMES", cards:["Rosanna","Billie Jean","Mickey","Amanda"] }
+  ]},
+  { id:105, title:"Puzzle 105", difficulty:"easy", groups:[
+    { connection:"U2 SONGS", cards:["Sunday Bloody Sunday","Pride (In the Name of Love)","With or Without You","Where the Streets Have No Name"] },
+    { connection:"INXS SONGS", cards:["Original Sin","Need You Tonight","New Sensation","Never Tear Us Apart"] },
+    { connection:"1980s AUSTRALIAN ACTS", cards:["Men at Work","Midnight Oil","Crowded House","The Church"] },
+    { connection:"1980s IRISH ACTS", cards:["U2","The Pogues","Sinéad O'Connor","The Waterboys"] }
+  ]},
+  { id:106, title:"Puzzle 106", difficulty:"medium", groups:[
+    { connection:"NEW ORDER SONGS", cards:["Blue Monday","Bizarre Love Triangle","True Faith","Temptation"] },
+    { connection:"JOY DIVISION SONGS", cards:["Love Will Tear Us Apart","Atmosphere","Transmission","She's Lost Control"] },
+    { connection:"FACTORY RECORDS ACTS", cards:["A Certain Ratio","Happy Mondays","The Durutti Column","Section 25"] },
+    { connection:"1980s BANDS WITH TWO-WORD NAMES", cards:["Sonic Youth","Echo & the Bunnymen","Cocteau Twins","Prefab Sprout"] }
+  ]},
+  { id:107, title:"Puzzle 107", difficulty:"easy", groups:[
+    { connection:"BRUCE SPRINGSTEEN SONGS", cards:["Hungry Heart","Dancing in the Dark","Glory Days","I'm on Fire"] },
+    { connection:"BON JOVI SONGS", cards:["Runaway","You Give Love a Bad Name","Livin' on a Prayer","Wanted Dead or Alive"] },
+    { connection:"NEW JERSEY ACTS", cards:["Bruce Springsteen","Bon Jovi","The Smithereens","Fugees"] },
+    { connection:"1980s SONGS ABOUT WORK", cards:["Working for the Weekend","9 to 5","She Works Hard for the Money","Manic Monday"] }
+  ]},
+  { id:108, title:"Puzzle 108", difficulty:"medium", groups:[
+    { connection:"THE CARS SONGS", cards:["Shake It Up","Drive","You Might Think","Magic"] },
+    { connection:"THE POLICE SONGS", cards:["Don't Stand So Close to Me","Every Little Thing She Does Is Magic","Every Breath You Take","King of Pain"] },
+    { connection:"1980s POWER POP ACTS", cards:["The Romantics","The Plimsouls","The dB's","Marshall Crenshaw"] },
+    { connection:"1980s BANDS WITH VEHICLE-RELATED NAMES", cards:["The Cars","The Motels","The Railway Children","Transvision Vamp"] }
+  ]},
+  { id:109, title:"Puzzle 109", difficulty:"easy", groups:[
+    { connection:"A-HA SONGS", cards:["Take on Me","The Sun Always Shines on T.V.","Hunting High and Low","The Living Daylights"] },
+    { connection:"TEARS FOR FEARS SONGS", cards:["Mad World","Shout","Everybody Wants to Rule the World","Head over Heels"] },
+    { connection:"1980s SONGS WITH WORLD IN THE TITLE", cards:["Everybody Wants to Rule the World","World in Motion","It's the End of the World as We Know It","World Shut Your Mouth"] },
+    { connection:"1980s DUOS", cards:["Eurythmics","Wham!","Pet Shop Boys","Erasure"] }
+  ]},
+  { id:110, title:"Puzzle 110", difficulty:"medium", groups:[
+    { connection:"SIOUXSIE AND THE BANSHEES SONGS", cards:["Happy House","Spellbound","Cities in Dust","Peek-a-Boo"] },
+    { connection:"BAUHAUS SONGS", cards:["Bela Lugosi's Dead","She's in Parties","Kick in the Eye","Dark Entries"] },
+    { connection:"GOTHIC ROCK ACTS", cards:["The Sisters of Mercy","The Mission","Fields of the Nephilim","The March Violets"] },
+    { connection:"1980s SONGS WITH HOUSE IN THE TITLE", cards:["Happy House","House of Fun","Our House","Burning Down the House"] }
+  ]},
+  { id:111, title:"Puzzle 111", difficulty:"easy", groups:[
+    { connection:"MICHAEL JACKSON SONGS", cards:["Beat It","Billie Jean","Bad","Smooth Criminal"] },
+    { connection:"WHITNEY HOUSTON SONGS", cards:["How Will I Know","Greatest Love of All","I Wanna Dance with Somebody","Didn't We Almost Have It All"] },
+    { connection:"1980s NUMBER ONE SINGLES", cards:["Faith","Heaven Is a Place on Earth","Kokomo","Straight Up"] },
+    { connection:"1980s SONGS WITH DANCE OR DANCING", cards:["Dancing in the Dark","I Wanna Dance with Somebody","Safety Dance","Dancing with Myself"] }
+  ]},
+  { id:112, title:"Puzzle 112", difficulty:"medium", groups:[
+    { connection:"PIXIES SONGS", cards:["Gigantic","Where Is My Mind?","Monkey Gone to Heaven","Here Comes Your Man"] },
+    { connection:"SONIC YOUTH SONGS", cards:["Teen Age Riot","Silver Rocket","Schizophrenia","Candle"] },
+    { connection:"1980s AMERICAN ALTERNATIVE BANDS", cards:["Dinosaur Jr.","Meat Puppets","Minutemen","Butthole Surfers"] },
+    { connection:"1980s SST RECORDS ACTS", cards:["Black Flag","Hüsker Dü","Minutemen","Meat Puppets"] }
+  ]},
+  { id:113, title:"Puzzle 113", difficulty:"easy", groups:[
+    { connection:"GEORGE MICHAEL SONGS", cards:["Faith","Father Figure","One More Try","Monkey"] },
+    { connection:"CYNDI LAUPER SONGS", cards:["Girls Just Want to Have Fun","Time After Time","She Bop","True Colors"] },
+    { connection:"1980s SONGS WITH TIME IN THE TITLE", cards:["Time After Time","If I Could Turn Back Time","Got the Time","Time Stand Still"] },
+    { connection:"1980s ACTS WITH GEORGE IN THE NAME", cards:["George Michael","Boy George","George Thorogood","Culture Club"] }
+  ]},
+  { id:114, title:"Puzzle 114", difficulty:"medium", groups:[
+    { connection:"THE REPLACEMENTS SONGS", cards:["I Will Dare","Bastards of Young","Alex Chilton","Can't Hardly Wait"] },
+    { connection:"HÜSKER DÜ SONGS", cards:["Celebrated Summer","Makes No Sense at All","Don't Want to Know If You Are Lonely","Could You Be the One?"] },
+    { connection:"MINNEAPOLIS 1980s ACTS", cards:["Prince","The Replacements","Hüsker Dü","Soul Asylum"] },
+    { connection:"1980s ALBUMS PRODUCED BY JIMMY IOVINE", cards:["Making Movies","Bella Donna","Under a Blood Red Sky","Once Upon a Time"] }
+  ]},
+  { id:115, title:"Puzzle 115", difficulty:"easy", groups:[
+    { connection:"RUN-D.M.C. SONGS", cards:["It's Like That","King of Rock","Walk This Way","It's Tricky"] },
+    { connection:"BEASTIE BOYS SONGS", cards:["Fight for Your Right","No Sleep till Brooklyn","Paul Revere","Brass Monkey"] },
+    { connection:"1980s HIP-HOP ACTS", cards:["Public Enemy","LL Cool J","Salt-N-Pepa","Eric B. & Rakim"] },
+    { connection:"DEF JAM ACTS OF THE 1980s", cards:["LL Cool J","Beastie Boys","Public Enemy","Slick Rick"] }
+  ]},
+  { id:116, title:"Puzzle 116", difficulty:"easy", groups:[
+    { connection:"TINA TURNER SONGS", cards:["What's Love Got to Do with It","Private Dancer","We Don't Need Another Hero","The Best"] },
+    { connection:"PAT BENATAR SONGS", cards:["Hit Me with Your Best Shot","Love Is a Battlefield","We Belong","Invincible"] },
+    { connection:"1980s FEMALE ROCK SINGERS", cards:["Joan Jett","Chrissie Hynde","Ann Wilson","Stevie Nicks"] },
+    { connection:"1980s SONGS WITH LOVE IN THE TITLE", cards:["What's Love Got to Do with It","Love Is a Battlefield","The Power of Love","I Want to Know What Love Is"] }
+  ]},
+  { id:117, title:"Puzzle 117", difficulty:"medium", groups:[
+    { connection:"THE PSYCHEDELIC FURS SONGS", cards:["Pretty in Pink","Love My Way","The Ghost in You","Heaven"] },
+    { connection:"ECHO & THE BUNNYMEN SONGS", cards:["The Killing Moon","The Cutter","Lips Like Sugar","Bring on the Dancing Horses"] },
+    { connection:"1980s LIVERPOOL ACTS", cards:["Frankie Goes to Hollywood","A Flock of Seagulls","The Icicle Works","Dead or Alive"] },
+    { connection:"1980s SONGS USED AS JOHN HUGHES FILM TITLES", cards:["Pretty in Pink","Some Kind of Wonderful","She's Having a Baby","Weird Science"] }
+  ]},
+  { id:118, title:"Puzzle 118", difficulty:"easy", groups:[
+    { connection:"GUNS N' ROSES SONGS", cards:["Welcome to the Jungle","Paradise City","Sweet Child o' Mine","Patience"] },
+    { connection:"MÖTLEY CRÜE SONGS", cards:["Shout at the Devil","Home Sweet Home","Girls, Girls, Girls","Dr. Feelgood"] },
+    { connection:"1980s LOS ANGELES ROCK BANDS", cards:["Guns N' Roses","Mötley Crüe","Ratt","Poison"] },
+    { connection:"1980s SONGS WITH HOME IN THE TITLE", cards:["Home Sweet Home","Take Me Home Tonight","Home by the Sea","Coming Home"] }
+  ]},
+  { id:119, title:"Puzzle 119", difficulty:"medium", groups:[
+    { connection:"XTC SONGS", cards:["Senses Working Overtime","Dear God","Mayor of Simpleton","Making Plans for Nigel"] },
+    { connection:"THE JAM SONGS", cards:["Going Underground","Start!","Town Called Malice","That's Entertainment"] },
+    { connection:"1980s BRITISH ALTERNATIVE ACTS", cards:["The The","The Fall","Wire","The Wedding Present"] },
+    { connection:"BANDS WITH THREE-LETTER NAMES", cards:["XTC","ABC","INXS","REM"] }
+  ]},
+  { id:120, title:"Puzzle 120", difficulty:"easy", groups:[
+    { connection:"EURYTHMICS SONGS", cards:["Sweet Dreams (Are Made of This)","Here Comes the Rain Again","Would I Lie to You?","Missionary Man"] },
+    { connection:"BILLY IDOL SONGS", cards:["White Wedding","Rebel Yell","Eyes Without a Face","Mony Mony"] },
+    { connection:"1980s SONGS WITH EYES IN THE TITLE", cards:["Eyes Without a Face","Private Eyes","Bette Davis Eyes","Hungry Eyes"] },
+    { connection:"1980s ACTS WITH BILLY IN THE NAME", cards:["Billy Idol","Billy Joel","Billy Ocean","Billy Bragg"] }
+  ]},
+  { id:121, title:"Puzzle 121", difficulty:"medium", groups:[
+    { connection:"KATE BUSH SONGS", cards:["Babooshka","Running Up That Hill","Cloudbusting","Hounds of Love"] },
+    { connection:"PETER GABRIEL SONGS", cards:["Games Without Frontiers","Shock the Monkey","Sledgehammer","In Your Eyes"] },
+    { connection:"1980s ARTISTS WHO WORKED WITH KATE BUSH", cards:["Peter Gabriel","David Gilmour","Prince","Roy Harper"] },
+    { connection:"1980s SONGS WITH MONKEY IN THE TITLE", cards:["Shock the Monkey","Brass Monkey","Monkey","Monkey Gone to Heaven"] }
+  ]},
+  { id:122, title:"Puzzle 122", difficulty:"easy", groups:[
+    { connection:"AEROSMITH 1980s SONGS", cards:["Dude (Looks Like a Lady)","Angel","Rag Doll","Janie's Got a Gun"] },
+    { connection:"ZZ TOP SONGS", cards:["Gimme All Your Lovin'","Sharp Dressed Man","Legs","Sleeping Bag"] },
+    { connection:"1980s ROCK TRIOS", cards:["Rush","ZZ Top","The Police","Motörhead"] },
+    { connection:"1980s SONGS WITH CLOTHING IN THE TITLE", cards:["Sharp Dressed Man","Raspberry Beret","Red Red Wine","Blue Jean"] }
+  ]},
+  { id:123, title:"Puzzle 123", difficulty:"medium", groups:[
+    { connection:"THE JESUS AND MARY CHAIN SONGS", cards:["Just Like Honey","April Skies","Head On","Happy When It Rains"] },
+    { connection:"COCTEAU TWINS SONGS", cards:["Pearly-Dewdrops' Drops","Lorelei","Carolyn's Fingers","Heaven or Las Vegas"] },
+    { connection:"4AD ACTS OF THE 1980s", cards:["Bauhaus","Dead Can Dance","Throwing Muses","Pixies"] },
+    { connection:"1980s SCOTTISH ALTERNATIVE ACTS", cards:["The Jesus and Mary Chain","Cocteau Twins","The Blue Nile","Aztec Camera"] }
+  ]},
+  { id:124, title:"Puzzle 124", difficulty:"easy", groups:[
+    { connection:"BONNIE TYLER SONGS", cards:["Total Eclipse of the Heart","Holding Out for a Hero","It's a Heartache","Have You Ever Seen the Rain?"] },
+    { connection:"HALL & OATES SONGS", cards:["You Make My Dreams","Private Eyes","Maneater","Out of Touch"] },
+    { connection:"1980s SONGS WITH HEART IN THE TITLE", cards:["Total Eclipse of the Heart","Hungry Heart","Open Your Heart","Owner of a Lonely Heart"] },
+    { connection:"1980s DUOS", cards:["Hall & Oates","Soft Cell","Wham!","Eurythmics"] }
+  ]}
+);
