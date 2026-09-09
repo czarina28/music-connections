63102830c102c5b212fc3594f9d6e0cdab4f9e4e
czarina28 <maria@mariaworld.com>
2026-09-09 11:57:01 +0200

    Add easy Puzzle 006

--- puzzles.js
+++ puzzles.js
@@ -4,219 +4,65 @@ const PUZZLES = [
         title: "Puzzle 001",
         difficulty: "easy",
         groups: [
-            {
-                connection: "YARDBIRDS GUITARISTS",
-                cards: [
-                    "Eric Clapton",
-                    "Jeff Beck",
-                    "Jimmy Page",
-                    "Top Topham"
-                ]
-            },
-            {
-                connection: "VELVET UNDERGROUND",
-                cards: [
-                    "Lou Reed",
-                    "John Cale",
-                    "Moe Tucker",
-                    "Sterling Morrison"
-                ]
-            },
-            {
-                connection: "BOWIE PERSONAS",
-                cards: [
-                    "Ziggy Stardust",
-                    "Aladdin Sane",
-                    "Thin White Duke",
-                    "Major Tom"
-                ]
-            },
-            {
-                connection: "WOODSTOCK PERFORMERS",
-                cards: [
-                    "Janis Joplin",
-                    "Jimi Hendrix",
-                    "Joe Cocker",
-                    "Richie Havens"
-                ]
-            }
+            { connection: "YARDBIRDS GUITARISTS", cards: ["Eric Clapton", "Jeff Beck", "Jimmy Page", "Top Topham"] },
+            { connection: "VELVET UNDERGROUND", cards: ["Lou Reed", "John Cale", "Moe Tucker", "Sterling Morrison"] },
+            { connection: "BOWIE PERSONAS", cards: ["Ziggy Stardust", "Aladdin Sane", "Thin White Duke", "Major Tom"] },
+            { connection: "WOODSTOCK PERFORMERS", cards: ["Janis Joplin", "Jimi Hendrix", "Joe Cocker", "Richie Havens"] }
         ]
     },
-
-   {
-    id: 2,
-    title: "Puzzle 002",
-    difficulty: "easy",
-    groups: [
-        {
-            connection: "FLEETWOOD MAC MEMBERS",
-            cards: [
-                "Stevie Nicks",
-                "Lindsey Buckingham",
-                "Christine McVie",
-                "Mick Fleetwood"
-            ]
-        },
-        {
-            connection: "ROXY MUSIC MEMBERS",
-            cards: [
-                "Bryan Ferry",
-                "Brian Eno",
-                "Phil Manzanera",
-                "Andy Mackay"
-            ]
-        },
-        {
-            connection: "BEATLES ALBUMS",
-            cards: [
-                "Revolver",
-                "Rubber Soul",
-                "Abbey Road",
-                "Let It Be"
-            ]
-        },
-        {
-            connection: "ROLLING STONES SONGS",
-            cards: [
-                "Angie",
-                "Satisfaction",
-                "Wild Horses",
-                "Paint It Black"
-            ]
-        }
-    ]
-},
-
+    {
+        id: 2,
+        title: "Puzzle 002",
+        difficulty: "easy",
+        groups: [
+            { connection: "FLEETWOOD MAC MEMBERS", cards: ["Stevie Nicks", "Lindsey Buckingham", "Christine McVie", "Mick Fleetwood"] },
+            { connection: "ROXY MUSIC MEMBERS", cards: ["Bryan Ferry", "Brian Eno", "Phil Manzanera", "Andy Mackay"] },
+            { connection: "BEATLES ALBUMS", cards: ["Revolver", "Rubber Soul", "Abbey Road", "Let It Be"] },
+            { connection: "ROLLING STONES SONGS", cards: ["Angie", "Satisfaction", "Wild Horses", "Paint It Black"] }
+        ]
+    },
     {
         id: 3,
         title: "Puzzle 003",
         difficulty: "medium",
         groups: [
-            {
-                connection: "MEMBERS OF THE POLICE",
-                cards: [
-                    "Sting",
-                    "Andy Summers",
-                    "Stewart Copeland",
-                    "Henry Padovani"
-                ]
-            },
-            {
-                connection: "TALKING HEADS",
-                cards: [
-                    "David Byrne",
-                    "Tina Weymouth",
-                    "Chris Frantz",
-                    "Jerry Harrison"
-                ]
-            },
-            {
-                connection: "LED ZEPPELIN MEMBERS",
-                cards: [
-                    "Robert Plant",
-                    "Jimmy Page",
-                    "John Paul Jones",
-                    "John Bonham"
-                ]
-            },
-            {
-                connection: "RAMONES",
-                cards: [
-                    "Joey",
-                    "Johnny",
-                    "Dee Dee",
-                    "Tommy"
-                ]
-            }
+            { connection: "MEMBERS OF THE POLICE", cards: ["Sting", "Andy Summers", "Stewart Copeland", "Henry Padovani"] },
+            { connection: "TALKING HEADS", cards: ["David Byrne", "Tina Weymouth", "Chris Frantz", "Jerry Harrison"] },
+            { connection: "LED ZEPPELIN MEMBERS", cards: ["Robert Plant", "Jimmy Page", "John Paul Jones", "John Bonham"] },
+            { connection: "RAMONES", cards: ["Joey", "Johnny", "Dee Dee", "Tommy"] }
         ]
     },
-
     {
         id: 4,
-    id: 4,
-    title: "Puzzle 004",
-    difficulty: "easy",
-    groups: [
-        {
-            connection: "TALKING HEADS MEMBERS",
-            cards: [
-                "David Byrne",
-                "Tina Weymouth",
-                "Chris Frantz",
-                "Jerry Harrison"
-            ]
-        },
-        {
-            connection: "DAVID BOWIE ALBUMS",
-            cards: [
-                "Low",
-                "Heroes",
-                "Lodger",
-                "Scary Monsters"
-            ]
-        },
-        {
-            connection: "WOODSTOCK PERFORMERS",
-            cards: [
-                "Jimi Hendrix",
-                "Janis Joplin",
-                "Joe Cocker",
-                "Richie Havens"
-            ]
-        },
-        {
-            connection: "SONGS WITH WOMEN'S NAMES",
-            cards: [
-                "Roxanne",
-                "Lola",
-                "Gloria",
-                "Layla"
-            ]
-        }
-    ]
+        title: "Puzzle 004",
+        difficulty: "easy",
+        groups: [
+            { connection: "TALKING HEADS MEMBERS", cards: ["David Byrne", "Tina Weymouth", "Chris Frantz", "Jerry Harrison"] },
+            { connection: "DAVID BOWIE ALBUMS", cards: ["Low", "Heroes", "Lodger", "Scary Monsters"] },
+            { connection: "WOODSTOCK PERFORMERS", cards: ["Jimi Hendrix", "Janis Joplin", "Joe Cocker", "Richie Havens"] },
+            { connection: "SONGS WITH WOMEN'S NAMES", cards: ["Roxanne", "Lola", "Gloria", "Layla"] }
+        ]
     },
-
     {
-    id: 5,
-    title: "Puzzle 005",
-    difficulty: "easy",
-    groups: [
-        {
-            connection: "RUSH ALBUMS",
-            cards: [
-                "2112",
-                "Hemispheres",
-                "Permanent Waves",
-                "Moving Pictures"
-            ]
-        },
-        {
-            connection: "GENESIS ALBUMS",
-            cards: [
-                "Foxtrot",
-                "Selling England by the Pound",
-                "The Lamb Lies Down on Broadway",
-                "A Trick of the Tail"
-            ]
-        },
-        {
-            connection: "FAMOUS LIVE ALBUMS",
-            cards: [
-                "Frampton Comes Alive!",
-                "Live at Leeds",
-                "At Folsom Prison",
-                "Cheap Trick at Budokan"
-            ]
-        },
-        {
-            connection: "THREE-LETTER BAND NAMES",
-            cards: [
-                "Yes",
-                "Can",
-                "XTC",
-                "ABC"
-            ]
-        }
-    ]
-}
+        id: 5,
+        title: "Puzzle 005",
+        difficulty: "easy",
+        groups: [
+            { connection: "RUSH ALBUMS", cards: ["2112", "Hemispheres", "Permanent Waves", "Moving Pictures"] },
+            { connection: "GENESIS ALBUMS", cards: ["Foxtrot", "Selling England by the Pound", "The Lamb Lies Down on Broadway", "A Trick of the Tail"] },
+            { connection: "FAMOUS LIVE ALBUMS", cards: ["Frampton Comes Alive!", "Live at Leeds", "At Folsom Prison", "Cheap Trick at Budokan"] },
+            { connection: "THREE-LETTER BAND NAMES", cards: ["Yes", "Can", "XTC", "ABC"] }
+        ]
+    },
+    {
+        id: 6,
+        title: "Puzzle 006",
+        difficulty: "easy",
+        groups: [
+            { connection: "THE WHO SONGS", cards: ["My Generation", "Pinball Wizard", "Baba O'Riley", "Behind Blue Eyes"] },
+            { connection: "FLEETWOOD MAC ALBUMS", cards: ["Rumours", "Tusk", "Mirage", "Tango in the Night"] },
+            { connection: "FAMOUS DRUMMERS", cards: ["Keith Moon", "John Bonham", "Ringo Starr", "Charlie Watts"] },
+            { connection: "SONGS WITH COLORS IN THE TITLE", cards: ["Purple Haze", "White Rabbit", "Paint It Black", "Blue Monday"] }
+        ]
+    }
 ];
